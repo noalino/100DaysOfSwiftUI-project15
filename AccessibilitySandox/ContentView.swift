19fde277a74c13b6a8a7ac250b74aa8bf2cbd6d0
Noalino <bgelineau.dev@gmail.com>
2024-02-22 14:21:07 +0100

    Add images

--- AccessibilitySandox/ContentView.swift
+++ AccessibilitySandox/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+
+    @State private var selectedPicture = Int.random(in: 0...3)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
     }
 }
 
