d96daef0ef2aa44f94c01f185566b7b111f58c75
Noalino <bgelineau.dev@gmail.com>
2024-02-22 14:05:54 +0100

    Initial Commit

---
+++ AccessibilitySandox/AccessibilitySandoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandoxApp.swift
+//  AccessibilitySandox
+//
+//  Created by Noalino on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
